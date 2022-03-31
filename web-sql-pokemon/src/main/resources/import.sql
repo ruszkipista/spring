f2fa5d885d2d97cf4969fafb0457ce3f1a4df4c0
ruszkipista <oi5672@gmail.com>
2022-03-31 22:01:33 +0100

    add DB initialization

---
+++ web-sql-pokemon/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO `pokemon` (`id`, `name`, `type`, `attack`, `hp`, `speed`) VALUES ('1', 'Pikachu', 'Electric', '10', '10', '9');
+
+INSERT INTO `pokemon` (`id`, `name`, `type`, `attack`, `hp`, `speed`) VALUES ('2', 'Squirtle', 'Water', '8', '10', '8');
+
+INSERT INTO `pokemon` (`id`, `name`, `type`, `attack`, `hp`, `speed`) VALUES ('3', 'Charmander', 'Fire', '9', '10', '7');
